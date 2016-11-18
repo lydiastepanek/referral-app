f9a509b6f81eaacfe05ca385923c3cd8d46c7789
Lydia Stepanek <lydia.stepanek@gmail.com>
2016-11-18 14:00:33 -0500

    ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,14 @@ class UsersController < ApplicationController
   before_action :require_login, only: [:show]
 
   def show
-    @referral_link = 'LINKLINK'
+    check_for_referral
+    @user = User.find(current_user.id)
+    @referral_link = @user.email
+  end
+
+  private
+
+  def check_for_referral
+    @refer_token = params[:referToken] if params[:referToken]
   end
 end
