a723329a96605cededb6c85b3f42e5fc864ff667
Lydia Stepanek <lydia.stepanek@gmail.com>
2016-11-17 19:55:07 -0500

    login is working

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113000750) do
+ActiveRecord::Schema.define(version: 20161118000920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                          null: false
+    t.string   "password_digest"
+    t.string   "session_token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "encrypted_password", limit: 128
+    t.string   "confirmation_token", limit: 128
+    t.string   "remember_token",     limit: 128
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
