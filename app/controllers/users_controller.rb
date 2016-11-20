9b1bee6e8cac5185f5eeb9abf3bb1fe2d2076891
Lydia Stepanek <lydia.stepanek@gmail.com>
2016-11-19 20:03:47 -0500

    start controller test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,15 @@ class UsersController < Clearance::UsersController
   before_action :require_login, only: [:show]
 
   def create
+    @user = user_from_params
     set_referrer
+
+    if @user.save
+      sign_in @user
+      redirect_back_or url_after_create
+    else
+      render template: "users/new"
+    end
   end
 
   def show
@@ -13,7 +21,8 @@ class UsersController < Clearance::UsersController
   def set_referrer
     if params[:referToken]
       token = URI.unescape(params[:referToken]).html_safe
-      user = User.find_referrer(token)
+      referrer = User.find_referrer(token)
+      @user.referrer = referrer
     end
   end
 end
