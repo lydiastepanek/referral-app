7b942b2188f2d691f303a0de0ad8c4eb4fcc21f4
Lydia Stepanek <lydia.stepanek@gmail.com>
2016-11-19 22:35:01 -0500

    unit test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+describe User do
+  subject { build(:user, :email => "example@test.com") }
+
+  it { is_expected.to have_many :referrals }
+  it { is_expected.to belong_to :referrer }
+
+  describe "referral link" do
+    it "contains the user's referral code" do
+      expect(subject.referral_link).to eq("/sign_up?refer_token=example@test.com")
+
+    end
+  end
+end
